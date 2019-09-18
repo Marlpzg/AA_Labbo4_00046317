f5fca0f03c0b3f4e288dc2fb6a58406de6b211d9
Mario <00046317@uca.edu.sv>
2019-09-18 10:06:00 -0600

    Ejercicio Completado

---
+++ Tarea.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+
+void swap(int* a, int* b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++){
+    
+        if (arr[j] < pivot){
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high){
+    if (low < high){
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main(){
+    int T, N;
+    scanf("%d", &T);
+    for(int i = 0; i < T; i++){
+        scanf("%d", &N);
+        int arr[N];
+
+        for(int j = 0; j < N; j++){
+            scanf("%d", &arr[j]);
+        }
+
+        int n = sizeof(arr) / sizeof(arr[0]);
+        quickSort(arr, 0, n - 1);
+        int currMult = 2;
+        for(int i = 0; i < n; i++){
+
+            if(arr[i] >= currMult){
+
+                if(arr[i]%2 != 0){
+                    int aux = arr[i];
+                    while (aux >= currMult){
+                        aux--;
+                    }
+                }
+
+                currMult+=2;
+            
+            }
+            
+        }
+        printf("%d\n", currMult);
+
+    }
+    return 0;
+}
